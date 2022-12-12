b8d437b688a46706a71f514cc56726ea0a1cbd30
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-12 11:53:01 +0300

    end task7

---
+++ task7/Program.cs
@@ -0,0 +1,7 @@
+﻿Random random = new Random();
+int number = random.Next(100, 1000);
+Console.WriteLine("Random number = {0}", number);
+int number1 = number/100;
+int number2 = number/10%10;
+int number3 = number%10;
+Console.WriteLine(number1*10+number3);
