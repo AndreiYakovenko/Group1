d9e9215cd16c5a52113549116180f92148b6f597
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-12 13:09:02 +0300

    end task9

---
+++ task9/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Please enter number");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a%23;
+int c = a%7;
+if (b==0 && c==0) Console.WriteLine("YES!");
+else Console.WriteLine("NO!");
