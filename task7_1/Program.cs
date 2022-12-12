285c7ff02a41b5eb28abb8739e55b7297f5ea8aa
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-12 12:30:58 +0300

    end task7_1

---
+++ task7_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Random random = new Random();
+int number = random.Next(100, 1000);
+Console.WriteLine($"Random number = {number}");
+Console.WriteLine("Please enter number position to delete");
+int delete = Convert.ToInt32(Console.ReadLine());
+var str = number.ToString();
+number = int.Parse(str.Remove(str.Length - delete, 1));
+Console.WriteLine(number);
+// считает цифры для удаления справа
