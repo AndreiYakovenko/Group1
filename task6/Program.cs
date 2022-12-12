eb4c8a31d26de842f87385a97d0d84a199d1c6d1
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-12 11:27:52 +0300

    end task6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int number = random.Next(10, 100);
+Console.WriteLine("Random number = {0}", number);
+int a = number / 10;
+int b = number % 10;
+if (a>b)
+Console.WriteLine("a>b, {0}", a);
+else if(b>a)
+Console.WriteLine("b>a, {0}", b);
+else
+Console.WriteLine("a==b, {0}", a);
