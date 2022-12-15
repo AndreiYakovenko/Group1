569697696fb99cb9080cdaffd306aaf0b5e70819
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-15 12:15:07 +0300

    end task17

---
+++ task17/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа, которая принимает на вход координаты точки  X Y,
+// и выдаёт номер четверти в которой находится эта точка,
+// причём X Y не равны нулю
+
+Console.WriteLine("Enter coordinate X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter coordinate Y");
+int y = Convert.ToInt32(Console.ReadLine());
+switch((x, y))
+{
+    case (>0, >0):
+        Console.WriteLine("The point is in the first quarter");
+        break;
+    case (<0, >0):
+        Console.WriteLine("The point is in the second quarter");
+        break;
+    case (<0, <0):
+        Console.WriteLine("The point is in the third quarter");
+        break;
+    case (>0, <0):
+        Console.WriteLine("The point is in the fourth quarter");
+        break;
+}
+
