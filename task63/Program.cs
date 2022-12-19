58b3fbfe8997735a5cd445cee16100830b40f71b
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-19 11:11:01 +0300

    create task63

---
+++ task63/Program.cs
@@ -0,0 +1,22 @@
+﻿// Нахождение факториала
+
+// int Fact(int num)
+// {
+//     Console.WriteLine(num);
+//     if (num==1) return 1;
+//     return num * Fact(num-1);
+// }
+// Console.WriteLine(Fact(6));
+
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.WriteLine("Please enter number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Row(num)
+{
+    int numbers
+    if (num == 1)
+}   
