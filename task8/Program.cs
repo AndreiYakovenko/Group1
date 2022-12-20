dd073770e8696d89a55559786612dace11239860
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-20 09:05:55 +0300

    formating

--- task8/Program.cs
+++ task8/Program.cs
@@ -2,5 +2,5 @@
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Please enter number 2");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a%b==0) Console.WriteLine("a multiple of b");
-else Console.WriteLine("not a multiple of b, remainder = " + a%b);
+if (a % b == 0) Console.WriteLine("a multiple of b");
+else Console.WriteLine("not a multiple of b, remainder = " + a % b);
