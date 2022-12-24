f57af3cba631f705c11aab25779ad4ccb5957faa
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-24 16:11:02 +0300

    end task39

---
+++ task39/Program.cs
@@ -0,0 +1,11 @@
+﻿// Переворот массива
+
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int help = 0;
+for (int i = 0; i < arr.Length / 2; i++)
+{
+    help = arr[i];
+    arr[i] = arr[arr.Length - 1 - i];
+    arr[arr.Length - 1 - i] = help;
+}
+Console.WriteLine(String.Join(" ", arr));
