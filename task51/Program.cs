44c59a10ceafd57dbef88dcbe79cf5b957db9e08
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-25 13:10:26 +0300

    end task51

---
+++ task51/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте двумерный массив. Найдите суму элементов, находящихся на главной диагонали (i=j)
+
+Console.WriteLine("Please enter the number of lines:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter the number of columns:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[m, n];
+
+void FillArray(int m, int n)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}" + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(m, n);
+PrintArray(array);
+Console.WriteLine();
+int sum = 0;
+for (int i = 0; i<array.GetLength(0); i++)
+{
+    for (int j = 0; j<array.GetLength(1); j++)
+    {
+        if (i == j) sum = sum + array[i,j];
+    }
+}
+Console.WriteLine("sum = " + sum);
