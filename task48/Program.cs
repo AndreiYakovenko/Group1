ff5a930e48e56b6f61936a5552821b6918f50450
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-25 11:05:35 +0300

    end task48

---
+++ task48/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте двумерный массив m на nб каждый элемент в массиве,
+// каждый элемент находится по формуле Amn = m + n
+
+Console.WriteLine("Please enter the number of lines:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter the number of columns:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = i + j;
+        Console.Write($"{array[i, j]}" + " ");
+    }
+    Console.WriteLine();
+}
